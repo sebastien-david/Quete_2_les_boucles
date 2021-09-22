8c9922938320f02a424863059b9c13e54c30a166
sebastien-david <90146791+sebastien-david@users.noreply.github.com>
2021-09-22 08:15:13 +0200

    Update quete 2 boucles

--- quete_2_boucles.cs
+++ quete_2_boucles.cs
@@ -10,13 +10,25 @@ namespace quete_2_boucles
     {
         static void Main(string[] args)
         {
-            string[] moves = new string[32];
-            int i = 0;
-            while ()
+                string[] moves = new string[32];
             for (int i = 0; i < moves.Length; i++)
             {
                 Console.WriteLine("enter dance move : ");
-                moves[i] = Console.ReadLine();
+                string dancemove = Console.ReadLine();
+                if (dancemove != "done")
+                {
+                    moves[i] = dancemove;
+                }
+                else
+                {
+                  break;
+                }
+            }
+            Console.WriteLine("Choose the number of iteration ");
+            int iteration = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < iteration; i++)
+            {
+                Console.WriteLine("take the move dance : " + moves[i]);
             }
         }
     }
